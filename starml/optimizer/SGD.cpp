1566abd0d362c0b90351cff8b1ed889418902fe8
fan1997 <2479031300@qq.com>
2020-08-11 10:17:55 +0800

    Change some expression

--- starml/optimizer/SGD.cpp
+++ starml/optimizer/SGD.cpp
@@ -4,17 +4,14 @@ namespace starml {
 namespace optimizer {
 
 STARML_DEFINE_DISPATCHER(sgd_dispatcher);
-void sgd_op(Matrix& parameters, Matrix& grad, float lr) {
+void sgd_op(Matrix& parameters, const Matrix& grad, const float lr) {
   sgd_dispatcher(parameters, grad, lr);
 }
 
 void SGD::step(){
-  std::cout << "SGD step..." << '\n';
-  // this->get_parameters().print();
+  STARML_LOG(INFO) << "SGD step...";
   sgd_op(this->get_parameters(), this->get_grad(), this->get_learning_rate());
-  // this->get_parameters().print();
 }
-
 
 } // namespace optimizer
 } // namespace starml
